1dc4e692d107a1cbf79509cef426ee61fe7633dd
Jesse Brink <jbrink0925@gmail.com>
2021-06-30 22:47:53 -0400

    users can select their choice of story; adjust CSS, add route

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import re
+from choices import CHOICES
 from stories import story as my_story
 
 from flask import Flask, request, render_template, url_for, redirect
 
 app = Flask(__name__)
+DEFAULT_STORY = CHOICES['bball']
 
 
 @app.route('/home', methods=['GET', 'POST'])
@@ -20,3 +21,13 @@ def story():
     story_=my_story
     data=request.form
     return render_template('story.html', story_=story_, answers=data)
+
+
+@app.route('/choice', methods=['GET', 'POST'])
+def choice():
+    if request.method == 'GET':
+        return render_template('choice.html')
+    
+    my_story.template = CHOICES.get(request.form['choices']) or DEFAULT_STORY
+    return redirect(url_for('home'))
+    
---
+++ choices.py
@@ -0,0 +1,16 @@
+CHOICES = {
+    'bball': """I was playing basketball
+                at {place} with Michael Jordan.
+                The {adjective} {noun} was really
+                bad! They couldn't {verb} {plural_noun}!""",
+    
+    'school': """The other day before school I was at
+                 {place}. The {adjective} {noun} did 
+                 ten pushups! Another kid {verb} the
+                 {plural_noun}.""",
+    
+    'circus': """There was a circus at the {place}.
+                 The {adjective} {noun} could not 
+                 stop laughing!! The monkey {verb}
+                 the {plural_noun}."""
+}
